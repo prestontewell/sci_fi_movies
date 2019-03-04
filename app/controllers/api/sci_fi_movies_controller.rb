c6b65a3b8f5467b51427ca68f62077517d42063b
prestontewell <preston@tewellphoto.com>
2019-03-04 16:39:25 -0600

    add create view and finished create action in controller

--- app/controllers/api/sci_fi_movies_controller.rb
+++ app/controllers/api/sci_fi_movies_controller.rb
@@ -6,7 +6,13 @@ class Api::SciFiMoviesController < ApplicationController
   end
 
   def create
-    render "show.json.jbuilder"
+    @movie = SciFiMovie.new(
+      title: params[:title],
+      director: params[:director],
+      runtime: params[:runtime]
+      )
+    @movie.save
+    render 'show.json.jbuilder'
   end
 
   def show
@@ -14,5 +20,7 @@ class Api::SciFiMoviesController < ApplicationController
     @movie = SciFiMovie.find_by(id:movie_id)
     render 'show.json.jbuilder'
   end
+
+
 
 end
